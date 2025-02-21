8bb62338ad5a46d99a3ccc5f34528a90d20f5227
Asma Ghafoor <asmaghafoorofficial@gmail.com>
2025-02-21 15:15:40 +0500

    express js

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.join(__dirname, 'public');
+// app.use(express.static(publicPath));
+
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/profile', (req, res) => {
+    const users = {
+        name: 'asma',
+        email: 'asma@gmail.com',
+        city: 'Karachi',
+        skills: ['JavaScript', 'C#', 'React JS', 'Node JS']
+    }
+    res.render('profile', { users });
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help', (req, res) => {
+    res.sendFile(`${publicPath}/help.html`)
+})
+
+
+// app.get('', (req, res) => {
+//     // console.log("Data sent by browser", req.query.name)
+//     res.send(`<h1>hello , this is Home page</h1>
+//         <a href="/about" />Go to About page</a>
+//         `);
+// });
+
+// app.get('/about', (req, res) => {
+//     // res.send("hello , this is About page");
+//     res.send(`
+//             <input type="text" placeholder="User name" value="${req.query.name}" />
+//             <button>Click me</button>
+//             <a href="/" />Go to Home page</a>
+//         `)
+// });
+
+// app.get('/help', (req, res) => {
+//     res.send([
+//         {
+//             name: "asma",
+//             email: "asma@gmail.com"
+//         },
+//         {
+//             name: "asma ghafoor",
+//             email: "asma@gmail.com"
+//         }
+//     ]);
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.send("hello , this is Contact page");
+// });
+
+app.listen(5000);
